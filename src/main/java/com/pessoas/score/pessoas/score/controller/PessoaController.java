48d4f0f56d2b555ef8f5b2423f971f84a8f98dc3
Izabel Vidal <izabelvidal99@gmail.com>
2022-03-22 18:37:58 -0300

    controllers e banco de dados

--- src/main/java/com/pessoas/score/pessoas/score/controller/PessoaController.java
+++ src/main/java/com/pessoas/score/pessoas/score/controller/PessoaController.java
@@ -9,9 +9,7 @@ import com.pessoas.score.pessoas.score.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
